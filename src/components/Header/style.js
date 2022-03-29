908cc5383c30959e2341a6f98c13fdc9de05ec36
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-03-29 10:11:01 -0300

    feat: style header

---
+++ src/components/Header/style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;  
+  height: 70px;
+  background: #C6E2FF;
+  top: 0px;
+  display: flex;
+  justify-content: space-between;
+ 
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 72px;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Title = styled.h1`
+  color: #000;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 32px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const Button = styled.button`
+  padding: 5px 10px;
+`
