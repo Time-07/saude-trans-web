b94444a963b73c5cc655f71d79133f946f52d5ac
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-01 17:05:51 -0300

    feat:inserindo rotas de telas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
-import PageLogin from "../Pages/PageLogin"
+import PageLogin from "../Pages/Login"
+import PageCadastro from "../Pages/Cadastro"
+import Perfil from "../Pages/Perfil"
 
 function routes() {
   return(
     <Routes>
       <Route  path="/" element={ <Home />}/>
       <Route  path="/login" element={ <PageLogin />}/>
+      <Route  path="/login/cadastro" element={ <PageCadastro />}/>
+      <Route  path="/perfil" element={ <Perfil />}/>
     </Routes>
   )
 }
