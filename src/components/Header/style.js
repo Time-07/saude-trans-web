1478e9831b473c6d47062d4dd1609738aa188db5
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-01 17:04:39 -0300

    feat: style menu/header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,23 +1,72 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+
 export const Container = styled.div`
-  width: 100%;  
-  height: 70px;
-  background: #C6E2FF;
+  height: 104px;
+  background: #fff;
   top: 0px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-between;   
+  box-shadow: 4px 8px 4px rgba(0, 0, 0, 0.15);
+`;
+export const Logo = styled.img`
+  width: 140px;
+  height: 104px;
+  margin-left:120px;  
+`;
+
+export const Icone = styled.img`
+  transition: 0.5s;
+  width: 15px;
+    
+  img {
+    &:hover {
+      transform: rotate(-180deg);
+    }
+  }
  
 `;
-export const Wrapper = styled.div`
+export const Dropdown = styled.div`
+  margin-right: 120px;
   display: flex;
-  flex-direction: row;
-  max-width: 1024px;
-  width: 100%;
-  margin: 0 72px;
-  justify-content: space-between;
   align-items: center;
+  flex-direction: row;
+  gap:2px;
 `;
+
+
+export const Dropbtn = styled.button`
+  color: #000;
+  background: none;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;  
+  width: 252px;
+  height: 48px;
+  top: 28px;
+  border-radius: 10px;
+  padding: 8px 12px 8px 24px; 
+`;
+
+
+export const Dropdowncontent = styled.div` 
+  display:none;
+  padding: 16px 12px 16px 24px;
+  width: 200px;
+  height: 120px;
+  position: absolute;
+  background-color: #D7D7D7;
+  z-index: 1;
+  border-radius:10px;
+  a {
+    &:hover {
+      background-color: #f1f1f1 ;
+
+    }
+  }
+`;
+
 export const Title = styled.h1`
   color: #000;
   font-size: 28px;
@@ -28,6 +77,19 @@ export const Title = styled.h1`
   text-align: left;
 `;
 
-export const Button = styled.button`
-  padding: 5px 10px;
+export const Login = styled(Link)`
+  color: black;
+  padding: 12px;
+  text-decoration: none;
+  display: block; 
 `
+
+ /* .dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: #3e8e41;
+} */
