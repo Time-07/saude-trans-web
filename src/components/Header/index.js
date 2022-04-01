2df7900e71132aecf4650e9a4bc7911e4d1a040f
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-01 16:59:03 -0300

    feat: contruindo menu de navegação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { Container, Wrapper, Title, Button } from "./style";
-import { Link } from "react-router-dom"
+import { Container,  Logo, Login, Dropdown,Dropbtn, Dropdowncontent, Icone } from "./style";
+import LogoImg from "../../assets/logoo.jpeg"
+import Iconee from "../../assets/iconseta.png"
 
 
 
-function Home() {
+const Header = () => {
   return (
     <Container> 
-      <Wrapper> 
-      <Title>Sáude para pessoas Trans </Title>  
-      <Button>
-      <Link to="/login"> Login / Criar</Link>  
-      </Button>
-      </Wrapper>     
+    
+      <Logo src={LogoImg} alt="logoIoasys" /> 
+      
+      <Dropdown> 
+        <Dropbtn>Encontrar Profissional<Icone src={Iconee} alt="logoIoasys" />     
+          <Dropdowncontent className="container">
+            <Login to="/perfil"><Icone src={Iconee} alt="logoIoasys" />Profissional</Login>                
+          </Dropdowncontent>
+          </Dropbtn>          
+          <Dropbtn>Sou Profissional <Icone src={Iconee} alt="logoIoasys" />     
+          <Dropdowncontent className="container">
+            <Login to="/cadastro"><Icone src={Iconee} alt="logoIoasys" />  Cadastro</Login>                       
+            <Login to="/login"><Icone src={Iconee} alt="logoIoasys" />  Login</Login>              
+          </Dropdowncontent>
+          </Dropbtn>  
+        </Dropdown>  
+              
+       
     </Container>
   )
 }
 
-export default Home;
+export default Header;
