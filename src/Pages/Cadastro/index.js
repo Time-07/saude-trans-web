905c9bd1e0b36fff0fe40ca64c1f208bfa30064e
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-01 16:59:51 -0300

    feat: tela de cadastro provisória

---
+++ src/Pages/Cadastro/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const Cadastro = () => {
+  return (
+    <div>index</div>
+  )
+}
+export default Cadastro
---
+++ src/Pages/Cadastro/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TitleDoc = styled.h1`
+  color: #000;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 32px;
+  letter-spacing: 0em;
+  text-align: center; 
+`;
