2d549a7520177f106f65533b38fb64337af4b124
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-01 21:54:16 -0300

    feat: validação inputs, on submit navegacão perfil

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,70 +1,79 @@
-import React from "react";
-import {  Title, Form, Input , ButtonLogin, ContainerLogin, TextCadastro, WrapperLogin, WrapperInput, ContainerBtn, ContainerCriar, Logar, Cadastro, Alerta, EsquecerSenha } from "./style";
 import { useFormik } from "formik";
-import *as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
 import Header from "../../components/Header";
+import Input from "../../components/Input";
+import { Alerta, ButtonLogin, Cadastro, ContainerCriar, ContainerLogin, EsquecerSenha, Form, TextCadastro, Title, WrapperInput, WrapperLogin } from "./style";
 
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
     validationSchema: yup.object({
-      email: yup.string().required("O campo é obrigatório"),
-      password: yup.number().required("O campo é obrigatório").positive()
-      .integer(),
+      email: yup.string().email('Email Invalido').required("O campo é obrigatório"),
+      password: yup.string().required("O campo é obrigatório"),
     }),
     onSubmit: (values) => {
+   
+
       alert(JSON.stringify(values, null, 2))
+      navigate("/perfil")
     },
   });
+
+  
    return (  
      <>
      <Header />
       <ContainerLogin>
         <WrapperLogin>
-          <Form onSubmit={formik.handleSubmit}>
-          <Title>login</Title> 
-                          
-        <WrapperInput>         
-          <Input 
-          type="email"
-          id="email"
-          placeholder="@email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.email}          
-          />
-          
-          {formik.touched.email && formik.errors.email ? (
-        <Alerta>{formik.errors.email}</Alerta>
-      ) : null}
-         <Input 
-          type="password"
-          id="password"
-          placeholder="senha"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.password}
-          />
-           {formik.touched.password && formik.errors.password ? (
-        <Alerta>{formik.errors.password}</Alerta>
-      ) : null}
-      <EsquecerSenha>Esqueci minha senha</EsquecerSenha>
+          <Form onSubmit={formik.handleSubmit} noValidate>
+          <Title>login</Title>                         
+           <WrapperInput>         
+            <Input 
+             type="email"
+             id="email"
+             label="Email"
+             errors={formik.touched.email && formik.errors.email}
+             onChange={formik.handleChange}
+             onBlur={formik.handleBlur}
+             value={formik.values.email}          
+            />       
+            {formik.touched.email && formik.errors.email ? (
+              <Alerta>{formik.errors.email}</Alerta>
+            ) : null}
         </WrapperInput>
-        
-        <ContainerBtn>
-        <ButtonLogin>
-          <Logar to="/perfil">Entrar</Logar>
+     
+        <WrapperInput>   
+           <Input 
+            type="password"
+            id="password"
+            label="Senha"
+            errors={formik.touched.password && formik.errors.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <Alerta>{formik.errors.password}</Alerta>
+          ) : null}
+          <EsquecerSenha>Esqueci minha senha</EsquecerSenha>  
+        </WrapperInput>             
+      
+        <ButtonLogin type="submit">
+         Entrar
         </ButtonLogin>
-        </ContainerBtn>
+      
         <ContainerCriar>
           <TextCadastro> Ainda não tem uma conta?</TextCadastro> 
           <Cadastro to="/login/cadastro">Cadastre-se</Cadastro >              
-        </ContainerCriar>
-        
+        </ContainerCriar>        
           </Form >
       </WrapperLogin>     
       </ContainerLogin>
