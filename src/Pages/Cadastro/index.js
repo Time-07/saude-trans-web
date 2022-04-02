0d632099d72fed40c1e6c869bd41fc93d5070d70
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-01 21:53:08 -0300

    feat: pagina de cadsatro

--- src/Pages/Cadastro/index.js
+++ src/Pages/Cadastro/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from "../../components/Header"
+
 
  const Cadastro = () => {
   return (
-    <div>index</div>
+    <div>
+      <Header />
+     cadastro
+    </div>
+    
   )
 }
 export default Cadastro
