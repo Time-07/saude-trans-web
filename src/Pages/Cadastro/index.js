ff24afc33d84173a3048cbd7b0e48f9f2c3b41ab
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-06 15:33:50 -0300

    feat: começando contruir tela de cadastro

--- src/Pages/Cadastro/index.js
+++ src/Pages/Cadastro/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Header from "../../components/Header"
+import { ContainerCadastro, TitleDoc, Detalhe, Text, Profissionais, Medico, TextOpcao, OpcaoCadastro, Opcao, Psicologo } from "./style";
 
 
  const Cadastro = () => {
-  return (
-    <div>
-      <Header />
-     cadastro
-    </div>
-    
+  return (        
+  <>
+    <Header />
+      <ContainerCadastro> 
+        <TitleDoc>Cadastro</TitleDoc>
+          <Detalhe></Detalhe>
+          <Text>Antes de iniciarmos o Cadastro, selecione abaixo o seu tipo de atuação.</Text> 
+      <OpcaoCadastro>
+        <Profissionais>
+          <Opcao><Medico htmlFor=""></Medico></Opcao>    
+          <TextOpcao > Médico(a)</TextOpcao>         
+        </Profissionais> 
+        <Profissionais>
+          <Opcao><Psicologo htmlFor=""></Psicologo></Opcao>    
+          <TextOpcao>Psicólogo(a)</TextOpcao>         
+        </Profissionais>        
+        </OpcaoCadastro>    
+      </ContainerCadastro>
+  </>            
   )
-}
+ }
+    
+       
+
 export default Cadastro
