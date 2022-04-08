494f32246bc1890ba769a91a19acf4ed50d34e15
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-08 20:36:12 -0300

    feat: form psicologo iniciado

---
+++ src/Pages/Cadastro/Form/Psicologo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {  Profissionais, Medico, TextOpcao, Opcao, Psicologo } from "../style";
+
+
+ const PsicologoForm = () => {
+
+  return (        
+  <>
+       
+        <Profissionais>
+          <Opcao><Psicologo htmlFor=""></Psicologo></Opcao>    
+          <TextOpcao>Psicólogo(a)</TextOpcao>         
+        </Profissionais>        
+   
+  </>            
+  )
+ }
+    
+       
+
+export default PsicologoForm;
