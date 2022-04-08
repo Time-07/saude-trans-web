55c068ba06125682921f6795f25967a6ff86737e
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-08 20:35:47 -0300

    feat: form tipo de profissional , onclick para escolher qual o tipo

---
+++ src/Pages/Cadastro/Form/TipoProfissional.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useCadastro } from '../../../context/useCadastro';
+import {  Profissionais, Medico, TextOpcao, Opcao, Psicologo } from "../style";
+
+
+ const TipoProfissional = () => {
+  const {setStep, setData} = useCadastro()
+
+
+  const clickMedico = () =>{
+    setStep(2)
+    setData(state => ({...state , tipoProfissional: 'medico'}))
+  }
+
+  const clickPsicologo = () =>{
+    setStep(3)
+    setData(state => ({...state , tipoProfissional: 'psicologo'}))
+  }
+
+  return (        
+  <>
+        <Profissionais onClick={clickMedico}>
+          <Opcao><Medico htmlFor=""></Medico></Opcao>    
+          <TextOpcao > Médico(a)</TextOpcao>         
+        </Profissionais> 
+        <Profissionais onClick={clickPsicologo}>
+          <Opcao><Psicologo htmlFor=""></Psicologo></Opcao>    
+          <TextOpcao>Psicólogo(a)</TextOpcao>         
+        </Profissionais>        
+   
+  </>            
+  )
+ }
+    
+       
+
+export default TipoProfissional
