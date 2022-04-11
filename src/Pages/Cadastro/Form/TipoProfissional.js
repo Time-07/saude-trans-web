3f13a99f0bbe572fab63f74ce1dfa57647edc1c0
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-11 17:36:22 -0300

    feat:cadastro de opção de profissinal

--- src/Pages/Cadastro/Form/TipoProfissional.js
+++ src/Pages/Cadastro/Form/TipoProfissional.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { useCadastro } from '../../../context/useCadastro';
-import { Profissionais, Medico, TextOpcao, Opcao, Psicologo } from '../style';
+import {
+  Profissionais,
+  Medico,
+  TextOpcao,
+  Opcao,
+  Psicologo,
+  ContainerFormTipoProfissional,
+  Text,
+} from '../style';
 
 const TipoProfissional = () => {
   const { setStep, setData } = useCadastro();
@@ -17,18 +25,23 @@ const TipoProfissional = () => {
 
   return (
     <>
-      <Profissionais onClick={clickMedico}>
-        <Opcao>
-          <Medico htmlFor=''></Medico>
-        </Opcao>
-        <TextOpcao> Médico(a)</TextOpcao>
-      </Profissionais>
-      <Profissionais onClick={clickPsicologo}>
-        <Opcao>
-          <Psicologo htmlFor=''></Psicologo>
-        </Opcao>
-        <TextOpcao>Psicólogo(a)</TextOpcao>
-      </Profissionais>
+      <Text>
+        Antes de iniciarmos o Cadastro, selecione abaixo o seu tipo de atuação.
+      </Text>
+      <ContainerFormTipoProfissional>
+        <Profissionais onClick={clickMedico}>
+          <Opcao>
+            <Medico htmlFor="" />
+          </Opcao>
+          <TextOpcao> Médico(a)</TextOpcao>
+        </Profissionais>
+        <Profissionais onClick={clickPsicologo}>
+          <Opcao>
+            <Psicologo htmlFor="" />
+          </Opcao>
+          <TextOpcao>Psicólogo(a)</TextOpcao>
+        </Profissionais>
+      </ContainerFormTipoProfissional>
     </>
   );
 };
