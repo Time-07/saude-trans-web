e1e97a663bb0bfb70a17fb4cb233aba2be414cdd
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-15 16:13:44 -0300

    feat: finalização e validação parte apresentação da tela perfil

--- src/Pages/Perfil/Form/Apresentacao.js
+++ src/Pages/Perfil/Form/Apresentacao.js
@@ -1,14 +1,61 @@
 import React from 'react';
-import { Container, ButtonSalvar, TextoAtendimento, TextArea } from './style';
+import {
+  FormApresentacao,
+  ButtonSalvar,
+  TextoAtendimento,
+  TextArea,
+  ContainerButton,
+  ContainerTextArea,
+} from './style';
+import { useFormik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { Alerta } from '../../Login/style';
 
 const Apresentacao = () => {
+  const formik = useFormik({
+    initialValues: {
+      experiencia: '',
+      curriculo: '',
+    },
+    validationSchema: yup.object({
+      experiencia: yup.string().required('O campo é obrigatório'),
+      curriculo: yup.string().required('O campo é obrigatório'),
+    }),
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+
   return (
-    <Container>
+    <FormApresentacao onSubmit={formik.handleSubmit} noValidate>
       <TextoAtendimento>Apresentação</TextoAtendimento>
-      <TextArea>Experiência com pacientes trans</TextArea>
-      <TextArea>Currículo e/ou formação</TextArea>
-      <ButtonSalvar>Salvar</ButtonSalvar>
-    </Container>
+      <ContainerTextArea>
+        <TextArea
+          name="experiencia"
+          placeholder="Experiência com pacientes trans"
+          onChange={formik.handleChange}
+          value={formik.values.experiencia}
+        />
+        {formik.touched.experiencia && formik.errors.experiencia ? (
+          <Alerta>{formik.errors.experiencia}</Alerta>
+        ) : null}
+      </ContainerTextArea>
+      <ContainerTextArea>
+        <TextArea
+          name="curriculo"
+          placeholder="Currículo e/ou formação"
+          onChange={formik.handleChange}
+          value={formik.values.curriculo}
+        />
+        {formik.touched.curriculo && formik.errors.curriculo ? (
+          <Alerta>{formik.errors.curriculo}</Alerta>
+        ) : null}
+      </ContainerTextArea>
+
+      <ContainerButton>
+        <ButtonSalvar type="submit">Salvar</ButtonSalvar>
+      </ContainerButton>
+    </FormApresentacao>
   );
 };
 
