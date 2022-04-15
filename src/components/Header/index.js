c278926431efc0d6bfe1ea198491e61f888590f5
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-14 21:21:25 -0300

    feat: creação de menu dropdown

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,46 +1,43 @@
-import React from 'react';
-import {
-  Container,
-  Logo,
-  Login,
-  Dropdown,
-  Dropbtn,
-  Dropdowncontent,
-  Icone,
-} from './style';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { Container, Logo, Login, Dropdown, Wrapper, Icone } from './style';
 import LogoImg from '../../assets/logoo.jpeg';
 import Iconee from '../../assets/seta-abrir.svg';
 
 const Header = () => {
   return (
     <Container>
-      <Logo src={LogoImg} alt='logoIoasys' />
+      <Wrapper>
+        <Logo src={LogoImg} alt="logoIoasys" />
 
-      <Dropdown>
-        <Dropbtn>
-          Encontrar Profissional
-          <Icone src={Iconee} alt='logoIoasys' />
-          <Dropdowncontent className='container'>
-            <Login to='/perfil'>
-              <Icone src={Iconee} alt='logoIoasys' />
-              Profissional
-            </Login>
-          </Dropdowncontent>
-        </Dropbtn>
-        <Dropbtn>
-          Sou Profissional <Icone src={Iconee} alt='logoIoasys' />
-          <Dropdowncontent className='container'>
-            <Login to='/cadastro'>
-              <Icone src={Iconee} alt='logoIoasys' />
-              Cadastro
-            </Login>
-            <Login to='/login'>
-              <Icone src={Iconee} alt='logoIoasys' />
-              Login
-            </Login>
-          </Dropdowncontent>
-        </Dropbtn>
-      </Dropdown>
+        <Dropdown>
+          <div className="dropdown">
+            <span>
+              Encontrar Profissional
+              <Icone src={Iconee} alt="logoIoasys" />
+            </span>
+            <div className="dropdown-content">
+              <Login to="/perfil">
+                <Icone src={Iconee} alt="logoIoasys" />
+                Profissional
+              </Login>
+            </div>
+          </div>
+          <div className="dropdown">
+            Sou Profissional <Icone src={Iconee} alt="logoIoasys" />
+            <div className="dropdown-content">
+              <Login to="/cadastro">
+                <Icone src={Iconee} alt="logoIoasys" />
+                Cadastro
+              </Login>
+              <Login to="/login">
+                <Icone src={Iconee} alt="logoIoasys" />
+                Login
+              </Login>
+            </div>
+          </div>
+        </Dropdown>
+      </Wrapper>
     </Container>
   );
 };
