c2e70d98bef3d8b3662a48eddc1de0581a8d3b6a
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-14 23:39:07 -0300

    feat: form de contato da tela de perfil

--- src/Pages/Perfil/Form/Contato.js
+++ src/Pages/Perfil/Form/Contato.js
@@ -1,8 +1,71 @@
 import React from 'react';
-import { Container } from './style';
+import {
+  Container,
+  TextoContato,
+  ContainerInput,
+  Especialidades,
+  TitleEspecialista,
+  ButtonSalvar,
+} from './style';
+
+import Input from '../../../components/Input';
 
 const Contato = () => {
-  return <Container>Form 3</Container>;
+  return (
+    <Container>
+      <TextoContato>Informações de Contato</TextoContato>
+
+      <ContainerInput>
+        <Input
+          name="telefone"
+          type="text"
+          label="Telefone (Opcional)"
+          // errors={formik.touched.telefone && formik.errors.telefone}
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // value={formik.values.telefone}
+          // errorMsg={formik.errors.telefone}
+        />
+      </ContainerInput>
+      <ContainerInput>
+        <Input
+          name="whatsapp"
+          type="text"
+          label="WhatsApp"
+          // errors={formik.touched.whatsapp && formik.errors.whatsapp}
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // value={formik.values.whatsapp}
+          // errorMsg={formik.errors.whatsapp}
+        />
+      </ContainerInput>
+      <ContainerInput>
+        <Input
+          name="email"
+          type="email"
+          label="Email"
+          // errors={formik.touched.email && formik.errors.email}
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // value={formik.values.email}
+          // errorMsg={formik.errors.email}
+        />
+      </ContainerInput>
+      <ContainerInput>
+        <Input
+          name="linkedIn"
+          type="linkedIn"
+          label="LinkedIn (opcional)"
+          // errors={formik.touched.linkedIn && formik.errors.linkedIn}
+          // onChange={formik.handleChange}
+          // onBlur={formik.handleBlur}
+          // value={formik.values.linkedIn}
+          // errorMsg={formik.errors.linkedIn}
+        />
+      </ContainerInput>
+      <ButtonSalvar>Salvar</ButtonSalvar>
+    </Container>
+  );
 };
 
 export default Contato;
