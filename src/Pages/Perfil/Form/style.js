b1d8050807a3037d4f798a13d3f622961550c894
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-14 21:23:45 -0300

    feat: estilicação dos forms

---
+++ src/Pages/Perfil/Form/style.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 104px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+`;
