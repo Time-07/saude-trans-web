66f8c4481728769521cea84231145bde02097a57
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-15 16:14:10 -0300

    feat: finalização e validação parte contato da tela perfil

--- src/Pages/Perfil/Form/Contato.js
+++ src/Pages/Perfil/Form/Contato.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import {
-  Container,
+  FormContato,
   TextoContato,
   ContainerInput,
   Especialidades,
   TitleEspecialista,
   ButtonSalvar,
+  ContainerButton,
 } from './style';
-
+import { conselhoCelular } from '../../../util/maskInput';
+import { useFormik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import { Alerta } from '../../Login/style';
 import Input from '../../../components/Input';
 
 const Contato = () => {
+  const formik = useFormik({
+    initialValues: {
+      telefone: '',
+      whatsapp: '',
+      email: '',
+      linkedIn: '',
+    },
+    validationSchema: yup.object({
+      whatsapp: yup.string().required('O campo é obrigatório'),
+      email: yup
+        .string()
+        .email('Email Invalido')
+        .required('O campo é obrigatório'),
+    }),
+
+    onSubmit: async values => {
+      console.log(values);
+    },
+  });
+
   return (
-    <Container>
+    <FormContato onSubmit={formik.handleSubmit} noValidate>
       <TextoContato>Informações de Contato</TextoContato>
 
       <ContainerInput>
@@ -20,11 +44,11 @@ const Contato = () => {
           name="telefone"
           type="text"
           label="Telefone (Opcional)"
-          // errors={formik.touched.telefone && formik.errors.telefone}
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.telefone}
-          // errorMsg={formik.errors.telefone}
+          errors={formik.touched.telefone && formik.errors.telefone}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={conselhoCelular(formik.values.telefone)}
+          errorMsg={formik.errors.telefone}
         />
       </ContainerInput>
       <ContainerInput>
@@ -32,11 +56,11 @@ const Contato = () => {
           name="whatsapp"
           type="text"
           label="WhatsApp"
-          // errors={formik.touched.whatsapp && formik.errors.whatsapp}
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.whatsapp}
-          // errorMsg={formik.errors.whatsapp}
+          errors={formik.touched.whatsapp && formik.errors.whatsapp}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={conselhoCelular(formik.values.whatsapp)}
+          errorMsg={formik.errors.whatsapp}
         />
       </ContainerInput>
       <ContainerInput>
@@ -44,11 +68,11 @@ const Contato = () => {
           name="email"
           type="email"
           label="Email"
-          // errors={formik.touched.email && formik.errors.email}
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.email}
-          // errorMsg={formik.errors.email}
+          errors={formik.touched.email && formik.errors.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          errorMsg={formik.errors.email}
         />
       </ContainerInput>
       <ContainerInput>
@@ -56,15 +80,17 @@ const Contato = () => {
           name="linkedIn"
           type="linkedIn"
           label="LinkedIn (opcional)"
-          // errors={formik.touched.linkedIn && formik.errors.linkedIn}
-          // onChange={formik.handleChange}
-          // onBlur={formik.handleBlur}
-          // value={formik.values.linkedIn}
-          // errorMsg={formik.errors.linkedIn}
+          errors={formik.touched.linkedIn && formik.errors.linkedIn}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.linkedIn}
+          errorMsg={formik.errors.linkedIn}
         />
       </ContainerInput>
-      <ButtonSalvar>Salvar</ButtonSalvar>
-    </Container>
+      <ContainerButton>
+        <ButtonSalvar type="submit">Salvar</ButtonSalvar>
+      </ContainerButton>
+    </FormContato>
   );
 };
 
