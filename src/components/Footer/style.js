001d1a2ddf2a6cd2202d5b9dd2df563850385d21
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-17 00:34:28 -0300

    feat: style componente de Footer

---
+++ src/components/Footer/style.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const Logo = styled.img`
+  width: 169px;
+  height: 104px;
+`;
+export const ContainerFooter = styled.footer`
+  background: #744d83;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 284px;
+  padding-left: 120px;
+  padding-bottom: 131px;
+  padding-top: 49px;
+`;
+export const Sobre = styled.div`
+  width: 290px;
+  height: 112px;
+`;
+export const Conta = styled.div`
+  width: 282px;
+  height: 48px;
+`;
+export const TituloSobre = styled.h1`
+  font-family: 'Fredoka';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+`;
+
+export const TextSobre = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.4px;
+  color: #ffffff;
+  padding-top: 10px;
+  padding-bottom: 20px;
+`;
+
+export const OpcaoConta = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.4px;
+  text-decoration-line: underline;
+  color: #ffffff;
+`;
+
+export const Fim = styled.div`
+  height: 40px;
+  width: 100%;
+  background: #44314b;
+`;
+export const TextFim = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.5px;
+  color: #d9d9d9;
+  text-align: center;
+  padding-top: 10px;
+`;
