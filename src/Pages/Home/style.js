88a8d8bb40b13a173152a2e7c2a5fb3806a5457f
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-17 12:59:21 -0300

    feat: style tela Home

---
+++ src/Pages/Home/style.js
@@ -0,0 +1,201 @@
+import styled from 'styled-components';
+
+export const ContainerInicial = styled.main``;
+
+export const ContainerUm = styled.div`
+  width: 100%;
+  height: 420px;
+  background: #fff;
+  padding: 32px 48px;
+`;
+export const TituloHome = styled.h1`
+  width: 575px;
+  height: 88px;
+  font-family: 'Fredoka';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 44px;
+  color: #000000;
+  padding-top: 10px;
+`;
+export const TextHome = styled.p`
+  width: 743px;
+  height: 96px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 29px;
+  color: #555555;
+  padding-top: 20px;
+`;
+export const Buttonn = styled.button`
+  width: 189px;
+  height: 56px;
+  background: #fcf1f3;
+  border: 1px solid #f5a9b8;
+  box-sizing: border-box;
+  box-shadow: 2px 2px 10px 1px rgba(181, 181, 181, 0.33);
+  border-radius: 8px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: #481F26;
+  cursor: pointer;
+ 
+  
+  &:hover {
+    background: #F9D7DD;
+    }
+  }
+`;
+export const ContainerDois = styled.div`
+  width: 100%;
+  height: 470px;
+  background: #fff;
+  padding: 32px 48px;
+`;
+
+export const TituloHome2 = styled.h1`
+  width: 575px;
+  height: 44px;
+  font-family: 'Fredoka';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 44px;
+  color: #000000;
+`;
+export const ContainerdeUso = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+  padding: 74px 120px;
+`;
+export const PassoaPasso = styled.div`
+  width: 281px;
+  height: 247px;
+  background: #f5edf8;
+  border-radius: 32px;
+  padding: 32px 48px;
+`;
+export const Passos = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.5px;
+  color: #000000;
+`;
+export const TextoPassos = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.5px;
+  color: #000000;
+  width: 215px;
+  height: 144px;
+  padding-top: 10px;
+`;
+
+export const ContainerTres = styled.div`
+  width: 100%;
+  height: 491px;
+  background: #fff;
+  display: flex;
+  justify-content: space-around;
+  gap: 195px;
+`;
+export const TituloHome3 = styled.h1`
+  width: 575px;
+  height: 88px;
+  font-family: 'Fredoka';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 44px;
+  color: #000000;
+  padding-top: 10px;
+`;
+
+export const DadosInformativos = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Dados = styled.div`
+  display: flex;
+  width: 587px;
+  height: 127px;
+  background: #f5edf8;
+  border-radius: 16px;
+  padding: 10px;
+`;
+
+export const TituloDados = styled.h1`
+  font-family: 'Fredoka';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 44px;
+  color: #000000;
+  text-align: center;
+`;
+export const TextoDados = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.5px;
+  color: #000000;
+`;
+export const TextoDados2 = styled.p`
+  width: 329px;
+  height: 60px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.25px;
+  color: #000000;
+`;
+export const ContainerQuatro = styled.div`
+  width: 100%;
+  height: 450px;
+  background: #fefaff;
+  display: flex;
+  justify-content: space-around;
+  gap: 195px;
+`;
+export const ParaAjudar = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const PossoAjudar = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 520px;
+  height: 48px;
+`;
+export const TextoAjudar = styled.p`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #000000;
+  padding: 20px;
+`;
