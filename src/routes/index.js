863b193429288701ba420af1b39164de6725789c
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-04-19 22:25:04 -0300

    chamando rota privada

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Home from '../Pages/Home';
 import PageLogin from '../Pages/Login';
 import PageCadastro from '../Pages/Cadastro';
 import Perfil from '../Pages/Perfil';
+import RoutePrivate from './RoutePrivate';
 
 function routes() {
   return (
@@ -11,7 +12,7 @@ function routes() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<PageLogin />} />
       <Route path="/cadastro" element={<PageCadastro />} />
-      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/perfil" element={<RoutePrivate component={Perfil} />} />
     </Routes>
   );
 }
